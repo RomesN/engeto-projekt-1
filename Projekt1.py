39d8882131af8485b8dd0829566afccbede69bb6
Roman Németh <romannemeth1@gmail.com>
2021-12-19 11:26:27 +0100

    Zmeny na základe feedbacku 13.12.2021.

--- Projekt1.py
+++ Projekt1.py
@@ -1,23 +1,23 @@
-TEXTS = ["""
-Situated about 10 miles west of Kemmerer, 
-Fossil Butte is a ruggedly impressive 
-topographic feature that rises sharply 
-some 1000 feet above Twin Creek Valley 
-to an elevation of more than 7500 feet 
-above sea level. The butte is located just 
-north of US 30N and the Union Pacific Railroad, 
-which traverse the valley. """,
+TEXTS = [
+    """Situated about 10 miles west of Kemmerer, 
+    Fossil Butte is a ruggedly impressive 
+    topographic feature that rises sharply 
+    some 1000 feet above Twin Creek Valley 
+    to an elevation of more than 7500 feet 
+    above sea level. The butte is located just 
+    north of US 30N and the Union Pacific Railroad, 
+    which traverse the valley. """,
 
-         """At the base of Fossil Butte are the bright 
-red, purple, yellow and gray beds of the Wasatch 
-Formation. Eroded portions of these horizontal 
-beds slope gradually upward from the valley floor 
-and steepen abruptly. Overlying them and extending 
-to the top of the butte are the much steeper 
-buff-to-white beds of the Green River Formation, 
-which are about 300 feet thick.""",
+    """At the base of Fossil Butte are the bright 
+    red, purple, yellow and gray beds of the Wasatch 
+    Formation. Eroded portions of these horizontal 
+    beds slope gradually upward from the valley floor 
+    and steepen abruptly. Overlying them and extending 
+    to the top of the butte are the much steeper 
+    buff-to-white beds of the Green River Formation, 
+    which are about 300 feet thick.""",
 
-         """The monument contains 8198 acres and protects 
+    """The monument contains 8198 acres and protects 
 a portion of the largest deposit of freshwater fish 
 fossils in the world. The richest fossil fish deposits 
 are found in multiple limestone layers, which lie some 
@@ -29,7 +29,9 @@ garpike and stingray are also present."""
          ]
 
 # dict of registered users {user: pass}
-reg_users = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
+reg_users = {
+    "bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"
+            }
 
 # helping variable
 separator = "-" * 40
